b39a1cbf683753efb49ff2905be68b54c637cbf6
Romain Crocq-Marchand <romain.crocq-marchand@epitech.eu>
2025-12-11 14:03:20 +0100

    feat: add job to migrate and seed the database

--- resources.tf
+++ resources.tf
@@ -192,3 +192,89 @@ resource "kubernetes_secret_v1" "postgres" {
     DATABASE_URL = digitalocean_database_cluster.postgres.private_uri
   }
 }
+
+resource "kubernetes_job_v1" "corevia_migrate_job" {
+  metadata {
+    name = "corevia-migrate-lb"
+    labels = {
+      app = "corevia-server"
+    }
+  }
+
+  spec {
+    backoff_limit = 0
+
+    template {
+      metadata {
+        labels = {
+          app = "corevia-server"
+        }
+      }
+
+      spec {
+        restart_policy = "Never"
+
+        container {
+          name  = "migrate"
+          image = "registry.digitalocean.com/corevia/corevia:server-latest" # replace with your container
+
+          command = ["yarn", "db:migrate"]
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret_v1.postgres.metadata[0].name
+            }
+          }
+
+          env {
+            name  = "NODE_TLS_REJECT_UNAUTHORIZED"
+            value = "0"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_job_v1" "corevia_seed_job" {
+  metadata {
+    name = "corevia-seed-lb"
+    labels = {
+      app = "corevia-server"
+    }
+  }
+
+  spec {
+    backoff_limit = 0
+
+    template {
+      metadata {
+        labels = {
+          app = "corevia-server"
+        }
+      }
+
+      spec {
+        restart_policy = "Never"
+
+        container {
+          name  = "migrate"
+          image = "registry.digitalocean.com/corevia/corevia:server-latest" # replace with your container
+
+          command = ["yarn", "db:seed"]
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret_v1.postgres.metadata[0].name
+            }
+          }
+
+          env {
+            name  = "NODE_TLS_REJECT_UNAUTHORIZED"
+            value = "0"
+          }
+        }
+      }
+    }
+  }
+}
